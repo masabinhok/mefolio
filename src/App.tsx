914f5800812c48a4a0ca2ad7006a4c1b67b0ef8f
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-11-15 18:15:42 +0545

    remove portfolio as a route

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { ThemeProvider } from "./components/ThemeProvider"
 import Welcome from "./pages/Welcome"
-import Portfolio from "./pages/Portfolio"
 function App() {
   return (
     <ThemeProvider>
       <Router>
         <Routes>
           <Route path="/" element={<Welcome />} />
-          <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
       </Router>
     </ThemeProvider>
