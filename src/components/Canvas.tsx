df766842a08000564d9ebefadbd2cdf1d791e2e8
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-11-22 22:34:52 +0545

    add color feature and change brush feature input

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,5 @@
 import { useVisitor } from "@/context/VisitorContext";
+import { DeleteForever } from "@mui/icons-material";
 import { Separator } from "@radix-ui/react-separator";
 import React, { useEffect, useRef, useState } from "react"
 
@@ -7,6 +8,7 @@ const Canvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [isDrawing, setIsDrawing] = useState<boolean>
     (false);
+  const [color, setColor] = useState<string>('red')
 
   const startDrawing = (event: React.MouseEvent<HTMLCanvasElement>) => {
     const { offsetX, offsetY } = event.nativeEvent;
@@ -49,7 +51,7 @@ const Canvas: React.FC = () => {
 
     ctx.lineWidth = 1;
     ctx.lineCap = 'round'
-    ctx.strokeStyle = 'black'
+    ctx.strokeStyle = color
   }, [])
 
   const clearCanvas = () => {
@@ -60,22 +62,46 @@ const Canvas: React.FC = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
   }
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas.getContext('2d')
+    if (!ctx) return;
+    ctx.strokeStyle = color
+  }, [color])
+
   return (
     <div className="section max-md:hidden">
       <h2 className="text-base mb-3 text-center">Leave your mark, <span className="text-primary">
         {visitor}
       </span>
       </h2>
-      <nav className="flex items-center gap-3 bg-secondary p-3  rounded-xl text-sm ">
+      <nav className="flex items-center gap-3 bg-secondary text-primary p-3  rounded-xl text-sm ">
 
         <button onClick={clearCanvas} className="text-primary ">
-          Clear
+          <DeleteForever />
         </button>
-        {[1, 2, 3].map((width) => (
-          <button onClick={() => changeWidth(width)} className={`w-${width} h-${width} bg-background rounded-xl`}>
-            {" "}
-          </button>
-        ))}
+        <label className="flex gap-2 items-center">
+          Brush:
+          <input
+            type="range"
+            min="1"
+            max="10"
+            onChange={(e) => changeWidth(Number(e.target.value))}
+            className="w-10"
+          />
+        </label>
+
+        <label className="flex gap-2 items-center" htmlFor="">
+          <span>Color: </span>
+          <input
+            type="color"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            className={`bg-secondary cursor-pointer`}
+          />
+        </label>
+
       </nav>
       <Separator className="my-1 " />
       <canvas
