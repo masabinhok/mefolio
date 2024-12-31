482bb05be24d9299a441890553d0ef0e20b9777d
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-12-31 18:59:39 +0545

    add draggable functionality to VisitorsCount component and update dependencies

--- src/components/VisitorsCount.tsx
+++ src/components/VisitorsCount.tsx
@@ -1,12 +1,14 @@
 import { useVisitor } from "@/context/VisitorContext"
-
+import Draggable from 'react-draggable';
 
 
 const VisitorsCount = () => {
   const { visitorCount } = useVisitor();
 
   return (
-    <div className="fixed bottom-10 right-10 bg-gradient-to-r text-white px-6 py-2  text-sm from-pink-500 to-purple-500 cursor-grabbing rounded-full "> {visitorCount} visits</div>
+    <Draggable>
+      <div className="fixed bottom-10 shadow-inner shadow-pink-100 right-10 bg-gradient-to-r text-white px-6 py-2  text-sm from-pink-500 to-purple-500 cursor-grabbing rounded-full "> {visitorCount} visits</div>
+    </Draggable>
   )
 }
 
