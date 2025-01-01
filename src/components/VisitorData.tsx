0bce4d9d937cff0c131019be5abb05739749793f
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-01-01 21:27:02 +0545

    add framer-motion for animations in VisitorData component

--- src/components/VisitorData.tsx
+++ src/components/VisitorData.tsx
@@ -1,21 +1,45 @@
-import { useVisitor } from "@/context/VisitorContext"
-
+import { useVisitor } from "@/context/VisitorContext";
+import { motion } from "framer-motion";
 
 const VisitorData = () => {
   const { visitor, visitorCount } = useVisitor();
+
+  // Animation Variants
+  const textVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { staggerChildren: 0.2 } },
+  };
+
   return (
     <section className="section">
-      <div className="text-center text-green-500 text-sm">
-        {visitor.name} is visiting from {visitor.device.os} using a browser based on {' '}
-        {visitor.device.browser} from {" "}{visitor.location.city}, {visitor.location.country}.
-        | {visitor.location.latitude} | {visitor.location.longitude} |
-        <span>{" "} You are {visitor.visitorRank}th person to visit this site out of {visitorCount} total visitors.</span>
-      </div>
-      <div>
-
-      </div>
+      <motion.div
+        className="text-center text-green-500 text-sm"
+        variants={containerVariants}
+        initial="hidden"
+        whileInView="visible" // Start animation every time it's in view
+        viewport={{ amount: 0.5 }} // Triggers when 50% of the section is in view
+      >
+        <motion.p variants={textVariants}>
+          {visitor.name} is visiting from {visitor.device.os} using a browser
+          based on {visitor.device.browser} from{" "}
+          {visitor.location.city}, {visitor.location.country}.
+        </motion.p>
+        <motion.p variants={textVariants}>
+          Latitude: {visitor.location.latitude} | Longitude:{" "}
+          {visitor.location.longitude}
+        </motion.p>
+        <motion.span variants={textVariants}>
+          You are the {visitor.visitorRank}th person to visit this site out of{" "}
+          {visitorCount} total visitors.
+        </motion.span>
+      </motion.div>
     </section>
-  )
-}
+  );
+};
 
-export default VisitorData
+export default VisitorData;
