2ca1829ba11f6b2f029d4f53238dcf16c0792a36
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-01-01 19:52:52 +0545

    refactor visitor model to set default values for optional fields

--- server/visitorModel.js
+++ server/visitorModel.js
@@ -3,24 +3,25 @@ import mongoose from 'mongoose';
 const visitorSchema = new mongoose.Schema({
   name: { type: String, required: true },
   uuid: { type: String, required: true },
-  markURLs: [{ type: String, required: true }],
+  markURLs: [{ type: String, default: [] }],
   ip: { type: String, required: true },
   visitTime: { type: Date, default: Date.now },
   location: {
-    country: String,
-    city: String,
-    latitude: Number,
-    longitude: Number,
+    country: { type: String, default: 'Unknown' },
+    city: { type: String, default: 'Unknown' },
+    latitude: { type: Number, default: 0 },
+    longitude: { type: Number, default: 0 },
   },
   device: {
-    browser: { type: String, required: true },
-    os: { type: String, required: true },
-    type: { type: String }, 
+    browser: { type: String, default: 'Unknown' },
+    os: { type: String, default: 'Unknown' },
+    type: { type: String, default: 'Unknown' },
+    screenResolution: { type: String, default: 'Unknown' },
   },
   behavior: {
-    referrer: String,
-    timeSpent: Number, 
-    pagesVisited: [String],
+    referrer: { type: String, default: 'Direct' },
+    timeSpent: { type: Number, default: 0 }, // in seconds
+    pagesVisited: { type: [String], default: [] },
   },
 });
 
