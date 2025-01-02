f69e54ba9bff0ef9cd8e4d4eb724a0e55226d7f3
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-01-02 11:06:11 +0545

    add node-fetch for server ping functionality to keep the server awake

--- server/server.js
+++ server/server.js
@@ -4,11 +4,19 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import visitorRoutes from './visitorRoutes.js';
+import fetch from 'node-fetch';
 
 const PORT = process.env.PORT || 5000;
+const backendUrl = process.env.BACKEND_URL;
 const app = express();
 
 app.use(express.json());    
+
+setInterval(() => {
+  fetch(`${backendUrl}/ping`)
+    .then(() => console.log('Pinged the server to keep it awake'))
+    .catch((err) => console.error('Error pinging the server:', err));
+}, 5 * 60 * 1000);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
